c90ad91f5ac873d327af9c381cd5e9c39cd3dd5e
Maelle Colussi <maellecolussi@yahoo.fr>
2014-12-05 07:40:44 +0100

    prove part ok formula, sufficient with symmetry of formulas

--- MMusGenScala/test/harm/ConstraintsTest.scala
+++ MMusGenScala/test/harm/ConstraintsTest.scala
@@ -6,16 +6,24 @@ import gen.Constraints
 
 class ConstraintsTest extends FunSuite {
   test("exactlyOne") {
-    val d = Constraints.exactlyOne(List.range(0, 4) map { _ => boolVar() })
+    val cl = List.range(0, 4) map { _ => boolVar() }
+    val d = Constraints.exactlyOne(cl)
     val expStr = "" +
       "List(or(P_0, P_1, P_2, P_3), " +
       "or(not(P_0), not(P_1)), or(not(P_0), not(P_2)), or(not(P_0), not(P_3)), " +
       "or(not(P_1), not(P_2)), or(not(P_1), not(P_3)), or(not(P_2), not(P_3)))"
 
     assert(d.toString == expStr)
+
+    val res = solveForSatisfiability(and(d: _*))
+    assert(res != None)
+
+    val d2 = or(cl(0)) :: or(cl(1)) :: d
+    assert(solveForSatisfiability(and(d2: _*)) == None)
+
   }
 
-  test("exactlyOnePair") { //to run with "exactlyOne" test (for the incdices of bool vars)
+  test("exactlyOnePair") {
     val cl = List.range(0, 4) map { _ => boolVar() }
     val cp = (cl(0), cl(1)) :: (cl(1), cl(3)) :: (cl(2), cl(0)) :: Nil
     val cpp = cp map { _ => boolVar() }
@@ -28,6 +36,15 @@ class ConstraintsTest extends FunSuite {
       "or(not(P_10), P_6), or(not(P_10), P_4), or(P_10, not(P_6), not(P_4)), " +
       "or(P_8, P_9, P_10), " +
       "or(not(P_8), not(P_9)), or(not(P_8), not(P_10)), or(not(P_9), not(P_10)))"
-    assert(d.toString == expStr)
+    //to run with "exactlyOne" test (for the incdices of bool vars)
+    //assert(d.toString == expStr)
+
+    //prove c1 && c2 <=> p by contradiction
+    val dCToP = or(cl(0)) :: or(cl(1)) :: or(!cpp(0)) :: d //(c1 and c2) => !p
+    val dPToC = or(!cl(0)) :: or(!cl(1)) :: or(cpp(0)) :: d //p => !(c1 and c2) <=> p => !c1 || !c2
+
+    assert(solveForSatisfiability(and(dCToP: _*)) == None)
+    assert(solveForSatisfiability(and(dPToC: _*)) == None)
+
   }
 }
